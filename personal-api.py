629fc4fcaddb8da47c48b06f498657a4c2ae0566
Syed-Irfaan <108723187+Syed-Irfaan@users.noreply.github.com>
2025-03-09 14:27:43 +0530

    personal-api.py

--- personal-api.py
+++ personal-api.py
@@ -1,4 +1,19 @@
-Update this file to add your code
-/name - Should give your name
-/register_number - Should give your register_number
-/department - Should give your department
+from flask import Flask
+
+app =Flask(__name__)
+
+@app.route("/")
+def name():
+    return "Syed Irfaan"
+
+
+@app.route("/registerNumber")
+def register_number():
+    return "22IT048"
+
+@app.route("/department")
+def department():
+    return "IT(Information Technology)"
+
+if __name__ == "__main__":
+    app.run(debug=True)
